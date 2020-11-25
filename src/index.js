03872ca22fda16cf2715ce8be80ea75b8aa54281
Leel Usama <leelusama@gmail.com>
2020-11-25 14:27:36 +0500

    implement redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
 window.onload = () => {
   const React = require('react');
   const ReactDOM = require('react-dom');
-  const { useState } = React;
+  const Redux = require('redux');
+  const { Provider, useDispatch, useSelector } = require('react-redux');
 
+  /**
+   * Constants
+   */
+  const INCREMENT = 'INCREMENT';
+  const DECREMENT = 'DECREMENT';
+
+  /**
+   * Reducer
+   */
+  const initialState = 0;
+  function counterReducer(state = 0, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case INCREMENT:
+        return state + 1;
+      case DECREMENT:
+        return state - 1;
+      default:
+        return state;
+    }
+  }
+
+  /**
+   * Action Creators
+   */
+  function increment() {
+    return {
+      type: INCREMENT,
+    };
+  }
+
+  function decrement() {
+    return {
+      type: DECREMENT,
+    };
+  }
+
+  /**
+   * Store
+   */
+  const store = Redux.createStore(counterReducer);
+
+  /**
+   * React
+   */
   function App() {
     return (
       <React.Fragment>
@@ -12,29 +58,28 @@ window.onload = () => {
   }
 
   function Counter() {
-    const [count, setCount] = useState(0);
+    const dispatch = useDispatch();
+    const count = useSelector((state) => state);
 
     return (
       <section className='counter'>
         <h2>Counter</h2>
-        <button id='decrement' onClick={decrement}>
+        <button id='decrement' onClick={() => dispatch(decrement())}>
           Decrement
         </button>
         <span id='count'>{count}</span>
-        <button id='increment' onClick={increment}>
+        <button id='increment' onClick={() => dispatch(increment())}>
           Increment
         </button>
       </section>
     );
-
-    function increment() {
-      setCount((prevCount) => prevCount + 1);
-    }
-    function decrement() {
-      setCount((prevCount) => prevCount - 1);
-    }
   }
 
   const container = document.getElementById('app');
-  ReactDOM.render(<App />, container);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    container
+  );
 };
