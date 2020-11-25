feab6ff45732fa6006782963b051cade2e0f89c6
Leel Usama <leelusama@gmail.com>
2020-11-25 11:13:42 +0500

    add redux flow

--- src/index.js
+++ src/index.js
@@ -1,24 +1,69 @@
 window.onload = () => {
+  const Redux = require('redux');
+
   const incrementButton = document.getElementById('increment');
   const decrementButton = document.getElementById('decrement');
+  const countElement = document.getElementById('count');
+
+  /**
+   * Constants
+   */
+  const INCREMENT = 'INCREMENT';
+  const DECREMENT = 'DECREMENT';
+
+  /**
+   * Reducer
+   */
+  const initialState = 0;
+  function counterReducer(state = 0, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case INCREMENT:
+        return state + 1;
+      case DECREMENT:
+        return state - 1;
+      default:
+        return state;
+    }
+  }
+
+  /**
+   * Action Creators
+   */
+
+  function increment() {
+    return {
+      type: INCREMENT,
+    };
+  }
+
+  function decrement() {
+    return {
+      type: DECREMENT,
+    };
+  }
+
+  /**
+   * Store
+   */
+
+  const store = Redux.createStore(counterReducer);
+  store.subscribe(render);
+
+  /**
+   * Main
+   */
+  render();
 
   incrementButton.addEventListener('click', () => {
-    incrementCount();
+    store.dispatch(increment());
   });
 
   decrementButton.addEventListener('click', () => {
-    decrementCount();
+    store.dispatch(decrement());
   });
 
-  function incrementCount() {
-    const countElement = document.getElementById('count');
-    const count = Number(countElement.innerText);
-    countElement.innerText = count + 1;
-  }
-
-  function decrementCount() {
-    const countElement = document.getElementById('count');
-    const count = Number(countElement.innerText);
-    countElement.innerText = count - 1;
+  function render() {
+    countElement.innerText = store.getState().toString();
   }
 };
